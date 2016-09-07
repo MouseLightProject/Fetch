7f3109e4ea83129c46b5da90f599eaff0c3e89c4
Ackerman <ackermand@hhmi.org>
2016-09-07 14:34:06 -0400

    fix/AutotileAtFixedZ: Added checkbox to select whether or not to use current Z.

--- ui/AutoTileDockWidget.cpp
+++ ui/AutoTileDockWidget.cpp
@@ -12,12 +12,29 @@ namespace ui{
     w->setLayout(form);
     setWidget(w);
 
+	///// Use Current Z Checkbox Control
+	QCheckBox *b = new QCheckBox();
+	QStateMachine *lockmachine = new QStateMachine(this);
+	QState *doUseCurrentZ = new QState(),
+		*doNotUseCurrentZ = new QState();
+	doUseCurrentZ->addTransition(b, SIGNAL(stateChanged(int)), doNotUseCurrentZ);
+	doNotUseCurrentZ->addTransition(b, SIGNAL(stateChanged(int)), doUseCurrentZ);
+	doUseCurrentZ->assignProperty(b, "text", "Using Current Z");
+	doNotUseCurrentZ->assignProperty(b, "text", "Not Using Current Z");
+
+	lockmachine->addState(doUseCurrentZ);
+	lockmachine->addState(doNotUseCurrentZ);
+	b->setCheckState(Qt::Checked);
+	lockmachine->setInitialState(doUseCurrentZ);
+	lockmachine->start();
+
     parent->_autotile_zoffum_control->createLineEditAndAddToLayout(form);
     parent->_autotile_zmaxmm_control->createLineEditAndAddToLayout(form);
     parent->_autotile_timeoutms_control->createLineEditAndAddToLayout(form);
     parent->_autotile_chan_control->createLineEditAndAddToLayout(form);
     parent->_autotile_intensity_thresh_control->createLineEditAndAddToLayout(form);
     parent->_autotile_area_thresh_control->createLineEditAndAddToLayout(form);
+	form->addRow("Use Current Z: ", b);
 
     AgentControllerButtonPanel *btns = new AgentControllerButtonPanel(&parent->_scope_state_controller,&dc->auto_tile_task);
     form->addRow(btns);
